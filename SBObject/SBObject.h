83b17da50c3db23d6b8eeb629ef53064658dd631
shaobo10 <shaobo10@staff.sina.com.cn>
2019-01-13 22:22:40 +0800

    Object-1

---
+++ SBObject/SBObject.h
@@ -0,0 +1,20 @@
+//
+//  SBObject.h
+//  pod
+//
+//  Created by shaobo10 on 2019/1/13.
+//  Copyright © 2019 sina. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SBObject : NSObject
+
+- (NSString *)sbAddress;
+
+- (NSString *)sbName;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SBObject/SBObject.m
@@ -0,0 +1,19 @@
+//
+//  SBObject.m
+//  pod
+//
+//  Created by shaobo10 on 2019/1/13.
+//  Copyright © 2019 sina. All rights reserved.
+//
+
+#import "SBObject.h"
+
+@implementation SBObject
+- (NSString *)sbAddress {
+    return @"hulongyuan";
+}
+
+- (NSString *)sbName {
+    return @"ssb";
+}
+@end
